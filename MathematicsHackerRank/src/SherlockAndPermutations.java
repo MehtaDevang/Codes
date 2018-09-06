99c39356fec502e843d1e98c2b2322ddfc5efb59
MehtaDevang <devangmehta@iiitdmj.ac.in>
2018-09-06 13:11:51 +0530

    adding SherlockAndPermutations.java

---
+++ MathematicsHackerRank/src/SherlockAndPermutations.java
@@ -0,0 +1,29 @@
+import java.math.BigInteger;
+import java.util.*;
+public class SherlockAndPermutations {
+	public static BigInteger fact(int n) {
+		BigInteger val = new BigInteger("1");
+		while(n > 1) {
+			val = val.multiply(new BigInteger(n + ""));
+			n--;
+		}
+		return val;
+	}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = Integer.parseInt(scanner.nextLine().trim());
+		
+		for(int i = 0; i < t; i++) {
+			String[] str = scanner.nextLine().trim().split(" ");
+			int n = Integer.parseInt(str[0]);
+			int m = Integer.parseInt(str[1]);
+			BigInteger fact = fact(m+n-1);
+			BigInteger n_fact = fact(n);
+			BigInteger m_fact = fact(m-1);			
+			BigInteger result = fact.divide(n_fact.multiply(m_fact));
+//			result = result.mod(new BigInteger("1000000007"));
+			System.out.println(result.mod(new BigInteger("1000000007")));
+		}
+		scanner.close();
+	}
+}
